6b15f77b7eccbed16aad7b9452ee7d9cfbfefb9a
Din19062004 <haivietb9@gmail.com>
2026-02-19 23:15:40 +0700

    feat: move VND_TO_USE rate to constant file

--- src/main/java/com/hust/soict/aims/subsystems/paypal/CurrencyConverter.java
+++ src/main/java/com/hust/soict/aims/subsystems/paypal/CurrencyConverter.java
@@ -2,9 +2,10 @@ package com.hust.soict.aims.subsystems.paypal;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import com.hust.soict.aims.utils.MoneyConstant;
 
 final class CurrencyConverter {
-	private static final BigDecimal VND_TO_USD_RATE = new BigDecimal("0.000041");
+	private static final BigDecimal VND_TO_USD_RATE = new BigDecimal(MoneyConstant.VND_TO_USD_RATE);
 
 	static BigDecimal vndToUsd(BigDecimal vnd) {
 		return vnd.multiply(VND_TO_USD_RATE).setScale(2, RoundingMode.HALF_UP);
---
+++ src/main/java/com/hust/soict/aims/utils/MoneyConstant.java
@@ -0,0 +1,6 @@
+package com.hust.soict.aims.utils;
+
+public class MoneyConstant {
+    public static final double TAX = 0.1;
+    public static final double VND_TO_USD_RATE = 0.000041;
+}
