081cad3c2f2dfdc7062040d8f70c001a3c8d3dbb
Din19062004 <haivietb9@gmail.com>
2026-02-20 19:55:54 +0700

    refactor: delete unnecessary comments

--- src/main/java/com/hust/soict/aims/IPaymentQRCode.java
+++ src/main/java/com/hust/soict/aims/IPaymentQRCode.java
@@ -6,20 +6,6 @@ import com.hust.soict.aims.entities.payments.QRCodePaymentStatus;
 import com.hust.soict.aims.exceptions.PaymentException;
 
 public interface IPaymentQRCode {
-    
-    /**
-     * Generate QR code for payment
-     * @param order Order to generate QR code for
-     * @return QRCode object containing QR image and bank information
-     * @throws PaymentException if QR generation fails
-     */
     QRCode generateQRCode(Order order) throws PaymentException;
-    
-    /**
-     * Check payment status for an order
-     * @param order Order to check payment status
-     * @return PaymentStatus indicating current payment state
-     * @throws PaymentException if status check fails
-     */
     QRCodePaymentStatus checkPaymentStatus(Order order) throws PaymentException;
 }
